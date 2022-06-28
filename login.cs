6cc1d03c7fada9c5b571c3c4b5b20775ed28e8b8
RutvikAR <94962550+RutvikAR@users.noreply.github.com>
2022-06-28 12:12:11 +0530

    Add files via upload

---
+++ login.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace firstproject
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            regi3d regi3 = new regi3d();
+            regi3.Show();
+            this.Hide();
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
