f1f48017af8f1e29ab75b686edc41f49a85cdcc6
RutvikAR <94962550+RutvikAR@users.noreply.github.com>
2022-06-28 12:11:26 +0530

    form1

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace firstproject
+{
+    public partial class loading : Form
+    {
+        public loading()
+        {
+            InitializeComponent();
+        }
+
+        private void loading_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel2.Width += 3;
+            if (panel2.Width >= 429)
+            {
+                timer1.Stop();
+                login fm2 = new login();
+                fm2.Show();
+                this.Hide();
+            }
+        }
+    }
+}
